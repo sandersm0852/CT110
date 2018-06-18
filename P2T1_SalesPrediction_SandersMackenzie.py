979483093609e89a914ac8931b059358502570f0
sandersm0852 <40345318+sandersm0852@users.noreply.github.com>
2018-06-17 17:20:00 -0700

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_SandersMackenzie.py
@@ -0,0 +1,13 @@
+#CTI 110
+#P2T1 Sales Prediction
+#MacKenzie Sanders
+#June 17 2018
+
+Total_Sales= float(input('Enter the projected sales:'))
+
+#Calculate the profit as 23 percent of total sales
+profit= Total_Sales * 0.23
+
+#Display the profit
+print('the profit is $', format(profit, ',.2f'))
+
